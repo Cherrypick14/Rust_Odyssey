2ea0df8aecb45719760d3bda6e713bef894eb0dd
Cherrypick14 <Cherrypicklet@gmail.com>
2025-04-24 13:50:38 +0300

    feat: add ownership transfer example with return

---
+++ challenge2/target/debug/deps/ownership_example-a85b6ef45e60be5f.d
@@ -0,0 +1,5 @@
+/home/cheryl/Data-nerd/cheryl/newrustchallenge/Rust_Odyssey/challenge2/target/debug/deps/ownership_example-a85b6ef45e60be5f: src/main.rs
+
+/home/cheryl/Data-nerd/cheryl/newrustchallenge/Rust_Odyssey/challenge2/target/debug/deps/ownership_example-a85b6ef45e60be5f.d: src/main.rs
+
+src/main.rs:
---
+++ challenge2/target/debug/ownership_example.d
@@ -0,0 +1,1 @@
+/home/cheryl/Data-nerd/cheryl/newrustchallenge/Rust_Odyssey/challenge2/target/debug/ownership_example: /home/cheryl/Data-nerd/cheryl/newrustchallenge/Rust_Odyssey/challenge2/src/main.rs
