ba9101cfaf9b52482301aec267dc9bd61a4ab5eb
Cherrypick14 <cherrypicklet@gmail.com>
2025-06-17 09:23:15 +0300

    Update the comment with borrow checker

--- scope_memory_safety/src/main.rs
+++ scope_memory_safety/src/main.rs
@@ -3,7 +3,7 @@ fn main(){
 //   let s = String:: from("admin");
 //   println!("{}", s);
   let r = dangle();
-  println!("The dangle reference is: {}", r); // Unsafe and Rust will prevent it at compile time.
+  println!("The dangle reference is: {}", r); // Unsafe and Rust will prevent it at compile time.Will catch it using a borrower checker.
 }
 // println!("{}", s); // s is out of scope
 
