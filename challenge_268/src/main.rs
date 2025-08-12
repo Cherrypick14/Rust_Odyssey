069b0324d71a6b85e983edc98caf48318f08c1bf
Cherrypick14 <cherrypicklet@gmail.com>
2025-08-12 20:11:32 +0300

    chore: add sample usage for acronym generator

---
+++ challenge_268/src/main.rs
@@ -0,0 +1,8 @@
+
+use acronym::abbreviate;
+
+fn main() {
+    println!("{}", abbreviate("As Soon As Possible")); // ASAP
+    println!("{}", abbreviate("Liquid-crystal display")); // LCD
+    println!("{}", abbreviate("Thank George It's Friday!")); // TGIF
+}
